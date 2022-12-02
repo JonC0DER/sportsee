2eeb0c6f296fdcfbc173dea07dd045f7f62fdbe1
joncoder <joncoder.kamite@gmail.com>
2022-12-02 01:22:19 +0100

    création du composant barre de navigation vertical

---
+++ src/component/VnavBar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+function VnavBar() {
+
+    return (
+        <nav className="v-nav">
+            <ul>
+                <li>
+                    <NavLink to="/meditation">
+                        <i className="fas fa-hiking"></i>
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/natation">
+                        <i className="fas fa-swimmer"></i>
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/cyclisme">
+                        <i className="fas fa-biking"></i>
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/musculation">
+                        <i className="fas fa-dumbbell"></i>
+                    </NavLink>
+                </li>
+            </ul>
+            <h5 className="copyright">
+                Copyright, SportSee 2020
+            </h5>
+        </nav>
+    )
+}
+
+export default VnavBar;
