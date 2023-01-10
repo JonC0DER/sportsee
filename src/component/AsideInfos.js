902367c3cbde3b9295b907a5e7e973a60a5223b3
joncoder <joncoder.kamite@gmail.com>
2023-01-10 15:47:36 +0100

    Ajout de commentaires

--- src/component/AsideInfos.js
+++ src/component/AsideInfos.js
@@ -5,31 +5,36 @@ import React from 'react'
  * @param {*} param0 
  * @returns 
  */
-function AsideInfos({data}) {
+function AsideInfos({ data }) {
     //console.log(data)
     /**
     * this array contain informations to interact with the props
     * and set icons colors and text
-    */ 
+    */
     const asideData = [
         ["red", "fire", "Calories", "kCal", "calorieCount"],
         ["blue", "drumstick-bite", "Proteines", "g", "proteinCount"],
         ["yellow", "apple-alt", "Glucides", "g", "carbohydrateCount"],
         ["pink", "hamburger", "Lipides", "g", "lipidCount"]
     ]
+
+    /**
+     * mapping asideData array to set all 
+     * the entries in the component
+     */
     return (
         <aside>
-        {asideData.map((entry, key) =>
-            <div className="info" key={key}>
-                <span className={`icone ${entry[0]}`}>
-                    <i className={`fas fa-${entry[1]}`}></i>
-                </span>
-                <span className="units-info">
-                    <h3 className="units">{data[entry[4]]}{entry[3]}</h3>
-                    <p>{entry[2]}</p>
-                </span>
-            </div>
-        )}
+            {asideData.map((entry, key) =>
+                <div className="info" key={key}>
+                    <span className={`icone ${entry[0]}`}>
+                        <i className={`fas fa-${entry[1]}`}></i>
+                    </span>
+                    <span className="units-info">
+                        <h3 className="units">{data[entry[4]]}{entry[3]}</h3>
+                        <p>{entry[2]}</p>
+                    </span>
+                </div>
+            )}
         </aside>
     )
 }
