26763232fffc4bab69bb7dbd540fb55ba0dce4d9
joncoder <joncoder.kamite@gmail.com>
2023-01-10 15:48:27 +0100

    Ajout de la route pour la page d'accueil

--- src/component/RouterComponent.js
+++ src/component/RouterComponent.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from '../pages/Home';
+import ChooseUser from '../pages/ChooseUser';
 
 function RouterComponent() {
   return (
     <Routes>
-      <Route path="/user/:id" element={<Home  />}/>
+      <Route path="/" element={<ChooseUser />} />
+      <Route path="/user/:id" element={<Home />} />
     </Routes>
   )
 }
